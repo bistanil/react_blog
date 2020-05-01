ee001570ba9d13f6607479db859475b2ff45c3d8
Neeraj Amoli <neerajamoli@soarlogic.com>
2020-05-01 18:37:14 +0530

    allow all origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+    before_action :allow_cross_origin
+
+    private
+
+    def allow_cross_origin
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
 end
